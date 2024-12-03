01e8cb0050a7f77944aa4e6e7b04beab941e7b14
Alex Akushevich <aku@unc.edu>
2024-12-02 20:23:05 -0500

    Updated clean.R program to remove quotation mark issues

--- clean.R
+++ clean.R
@@ -1,3 +1,13 @@
 library("tidyverse")
-spotify_dataset<-read_csv("data/spotify_dataset.csv")
-write_csv(spotify_dataset,"data/spotify_dataset_clean.csv")
+library("glue")
+#import raw lines to remove mismatched quotes (e.g., [12" version])
+raw_data <- tibble(readLines("C:/Users/AlexA/OneDrive/Documents/UNC BIOS/BIOS 611/Project/data/spotify_dataset.csv"))
+colnames(raw_data) <- c("line")
+
+cleaned_data <- raw_data %>% filter(str_count(line, "\"") == 8) %>% as.tibble()
+removed_data <- raw_data %>% filter(str_count(line, "\"") != 8) %>% mutate(count=str_count(line, "\"")) %>% as.tibble()
+
+message(glue("{nrow(removed_data)} lines with excess quotations removed"))
+
+#write back to csv
+writeLines(cleaned_data$line, "C:/Users/AlexA/OneDrive/Documents/UNC BIOS/BIOS 611/Project/data/spotify_dataset_clean.csv")
